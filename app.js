a0cb365d22b76b1756bb5ce8f7ee81430017e857
dongjiayun <dongjiayun@tv189.com>
2017-08-25 14:56:18 +0800

    ver 1.2

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //app.js
+var wxQuery = require('./utils/wxquery.js');
 App({
   onLaunch: function() {
     //调用API从本地缓存中获取数据
@@ -25,5 +26,8 @@ App({
 
   globalData: {
     userInfo: null
-  }
+  },
+  /** 全局引入wxQuery */
+  wxQuery: wxQuery,
+  $: wxQuery.$,
 })
--- pages/live/index.js
+++ pages/live/index.js
@@ -9,6 +9,9 @@ var _merCode = '';
 var _isTop = '';
 var _userInfo='';
 var _player = '';
+var _app = getApp();
+var _comment = {};
+
 
 
 function getToken(){
@@ -18,6 +21,7 @@ function getToken(){
       var currentTime = new Date().getTime();
       if ((currentTime - res.data) > 86400000) {
           init();
+          getRoomId();
       }else{
         wx.getStorage({
           key: 'token',
@@ -33,6 +37,7 @@ function getToken(){
     fail: function () {
       if (_token == '') {
         init();
+        getRoomId();
       }
     },
   })
@@ -71,6 +76,17 @@ function getRoomDetail(){
     },
     success: function (res) {
       console.log(res.data);
+      if(!res.data.info){
+        wx.showToast({
+          title: '无此直播间',
+          icon: 'loading',
+          duration: 2000,
+        })
+        setTimeout(function () {
+          wx.navigateBack({
+          });},2000);
+        return;
+      }
       _roomInfo = res;
       _liveId = res.data.info.liveId;
       _merCode = res.data.info.merCode;
@@ -84,6 +100,7 @@ function getRoomDetail(){
       //queryUserInfo()
       getHistoryCm();
       webSocket('');
+      getComment(1, 10);
     }
   })
 }
@@ -111,13 +128,13 @@ function getLivePlayUrl(liveId){
     },
     success: function (res) {
       console.log(res.data);
+      _player.pause();
       var playUrl = res.data.info.playUrl.play.src.hls;
-      _page.videoContext = wx.createVideoContext('main_video_player');
-      _player = _page.videoContext;
-      _page.setData({
-        src:"http://live.tv189.com/portal_live/wap/test.mp4",
-        //src:playUrl,
-      }); 
+      // _page.setData({
+      //   //src:"http://live.tv189.com/portal_live/wap/test.mp4",
+      //   src:playUrl,
+      // }); 
+      playerController("video",playUrl);
       _page.videoContext.play();
     }
   })  
@@ -193,25 +210,47 @@ function getAgendaList() {
 
 function webSocket(uid){
   wx.connectSocket({
-    url: 'ws://live.tv189.com/w_sub',
+    url: 'wss://live.tv189.com/w_sub',
     data: {
       id: _liveId,
-      uid: uid,
     },
     header: {
       'content-type': 'application/json'
     },
     //protocols: ['protocol1'],
-    method: "GET",
-    success:function(){
+    success:function(res){
+          console.log(res.data);
           console.log('websocket connect successfully');
-          wx.onSocketMessage(function (res) {
-            var commend = res.data;
-            // var content = JSON.stringify(res.data);
-             console.log('收到服务器内容：' + commend);
-          })
     },
-  })
+  });
+  wx.onSocketError(function (res) {
+    console.log('WebSocket连接打开失败，请检查！')
+  });
+  wx.onSocketMessage(function (res) {
+    var commend = res.data;
+    var commend = JSON.parse(commend);
+    // console.log(commend.type);
+    // if(commend.type=='ping'){
+
+    // }
+    var cmd = commend.cmd;
+    console.log(commend);
+    var content = commend.content;
+    content = JSON.parse(content);
+    //console.log(content);
+    runSocketCm(cmd,content);
+    console.log('收到服务器内容：' + commend);
+  });
+  wx.onSocketClose(function (res) {
+    console.log('WebSocket 已关闭！')
+  });
+  wx.onSocketOpen(function () {
+    var login_data = '{"type":"login","liveId":"' + _liveId + '","admin":"' + _liveId + '"}';
+    console.log("websocket握手成功，发送登录数据:" + login_data); 
+    wx.sendSocketMessage({
+      data: login_data,
+    })
+  });
 }
 
 function getHistoryCm(){
@@ -226,9 +265,12 @@ function getHistoryCm(){
     },
     success: function (res) {
         var commend = res.data.info;
+        console.log('commend:'+commend);
         console.log(commend);
-        for(var cmd in commend){
-          runSocketCm(cmd);
+        for(var cmd in commend){ 
+          var content = commend[cmd];
+          //console.log(commend[cmd]);
+          runSocketCm(cmd,content);
         }       
     }
   })  
@@ -249,26 +291,105 @@ function queryUserInfo(){
   })
 }
 
-function runSocketCm(commend){
+function runSocketCm(commend,content){
    switch(commend){
      case 'menuChange':
+       var item_id = content.id;
+       console.log('commend.id+' + item_id);
+       //console.log('commend.id+' + item_id);
+       _page.setData({
+         isOnLoad: item_id,
+       });
        break;
      case 'menuFlush': getAgendaList();
        break;
      case 'play': 
        console.log('play!');
-       getLivePlayUrl(_liveId);
+       var type = content.type;
+       if (type == 'start'){
+         getLivePlayUrl(_liveId);
+       }else if(type == 'stop'){
+         _player.pause();
+         var url = content.url;
+         var type = content.source;
+         console.log('type='+type);
+         playerController(type,url);
+       }
        break;
+     case 'note':
+       console.log('note:' + content.note);
+      //  _player.sendDanmu({
+      //    text: content.note,
+      //    color: '#fff',
+      //  })
+      //  if(content.note){
+
+      //  }else{
+      //    _page.setData({
+      //      danmuList:'',
+      //    }); 
+      //  }
+     break;
    }
+}
+
+function addClass(obj, cls) {
+  if (!this.hasClass(obj, cls)) {
+    obj.className += " " + cls;
+  }
+}
+
+function removeClass(obj, cls) {
+  if (hasClass(obj, cls)) {
+    var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+    obj.className = obj.className.replace(reg, ' ');
+  }
+}
+function playerController(type,url){
+  _page.setData({
+    playerType:type,
+  });
+  _page.setData({
+    src:url,
+  })
+}
+
+function getComment(page,size){
+  wx.request({
+    url: _host + 'index.php?act=Comment&fun=getCommentList',
+    data: {
+      token: _token,
+      liveId:_liveId,
+      page:page,
+      size:size,
+    },
+    header: {
+      'content-type': 'application/json'
+    },
+    success: function (res) {
+      var comment = res.data.info.data;
+      _comment = Object.assign(_comment,comment);
+      console.log(_comment);
+      console.log("_comment________________");
+      _page.setData({
+        main_list_detail_comment:_comment,
+      });
+    }
+  }) 
 }
 
 
 Page({
   data:{
+    name: 'example',
     isTop:'../../img/icon01.png',
+    playerType:'video',
+    current_list_btn:'agenda',
   },
   onReady: function (e) {
     _page = this;
+    _page.videoContext = wx.createVideoContext('main_video_player');
+    _player = _page.videoContext;
     getToken();    
     //console.log('roomId=' +_roomId); 
   },
@@ -276,4 +397,14 @@ Page({
     console.log('addTop!');
     addTop();
   },
+  main_list_btn_agenda:function(){
+    this.setData({
+      current_list_btn:'agenda',
+    })
+  },
+  main_list_btn_comment:function(){
+    this.setData({
+      current_list_btn: 'comment',
+    })
+  }
 })
